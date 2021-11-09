a32ead396cd9301e99b73c562bced50d1b19f19f
aniruddh-23 <anicd01@gmail.com>
2021-11-09 12:53:55 +0530

    added vmsecond

--- main.tf
+++ main.tf
@@ -17,3 +17,18 @@ resource "google_compute_instance" "myvm2"{
                 }
         }
 }
+
+resource "google_compute_instance" "myvm3"{
+        name = "mysecondvm2"
+        machine_type = "f1-micro"
+        zone = "us-central1-a"
+        network_interface{
+                network = "default"
+                access_config{}
+        }
+        boot_disk {
+                initialize_params{
+                        image = "debian-cloud/debian-10"
+                }
+        }
+}
