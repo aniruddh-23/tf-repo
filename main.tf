073d8063c29e2eb9c7d63f2c7e662a036ca0b046
aniruddh-23 <anicd01@gmail.com>
2021-11-09 13:36:16 +0530

    added variable

--- main.tf
+++ main.tf
@@ -3,23 +3,12 @@ provider "google"{
         # credentials = "keys.json"
         region = "us-central1"
 }
-resource "google_compute_instance" "myvm2"{
-        name = "myfirstvm2"
-        machine_type = "f1-micro"
-        zone = "us-central1-a"
-        network_interface{
-                network = "default"
-                access_config{}
-        }
-        boot_disk {
-                initialize_params{
-                        image = "debian-cloud/debian-10"
-                }
-        }
-}
 
-resource "google_compute_instance" "myvm3"{
-        name = "mysecondvm2"
+variable project{
+        type="string"
+}
+resource "google_compute_instance" "myvm2"{
+        name = "myvm-${project}"
         machine_type = "f1-micro"
         zone = "us-central1-a"
         network_interface{
