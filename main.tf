6467ef8c2ff21a93f8c1f8aa0587069536381bfd
Aniruddh Chandra Dandriyal <47289626+aniruddh-23@users.noreply.github.com>
2021-11-09 13:54:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,15 @@
+variable "project"{
+        type=string
+}
 provider "google"{
-        project="excellent-zoo-323604"
+        project="${var.project}"
         # credentials = "keys.json"
         region = "us-central1"
 }
-resource "google_compute_instance" "myvm2"{
-        name = "myfirstvm2"
-        machine_type = "f1-micro"
-        zone = "us-central1-a"
-        network_interface{
-                network = "default"
-                access_config{}
-        }
-        boot_disk {
-                initialize_params{
-                        image = "debian-cloud/debian-10"
-                }
-        }
-}
 
-resource "google_compute_instance" "myvm3"{
-        name = "mysecondvm2"
+
+resource "google_compute_instance" "myvm2"{
+        name = "myvm-one"
         machine_type = "f1-micro"
         zone = "us-central1-a"
         network_interface{
